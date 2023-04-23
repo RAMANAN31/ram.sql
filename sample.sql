0bde7690e4f4e705c6cb4da26c581d9fccdc6863
RAMANAN <112418260+RAMANAN31@users.noreply.github.com>
2023-04-23 14:40:07 +0530

    Update sample.sql
    WHERE clause: The WHERE clause is used to filter rows based on a condition. For example, to select only the students in grade 10 from the “Students” table, you can use the following statement:
    SELECT * FROM Students
    WHERE Grade = 10;
    GROUP BY clause: The GROUP BY clause is used to group rows based on one or more columns. For example, to calculate the average GPA for each grade in the “Students” table, you can use the following statement:
    SELECT Grade, AVG(GPA)
    FROM Students
    GROUP BY Grade;
    HAVING clause: The HAVING clause is used to filter groups based on a condition. For example, to select only the grades with an average GPA greater than 3.5 from the “Students” table, you can use the following statement:
    SELECT Grade, AVG(GPA)
    FROM Students
    GROUP BY Grade
    HAVING AVG(GPA) > 3.5;
    ORDER BY clause: The ORDER BY clause is used to sort rows based on one or more columns. For example, to select all students from the “Students” table and sort them by last name in ascending order, you can use the following statemen

--- sample.sql
+++ sample.sql
@@ -14,3 +14,18 @@ INSERT INTO Students (StudentID, FirstName, LastName, Grade, GPA)
 VALUES (1, 'John', 'Doe', 9, 3.5),
        (2, 'Jane', 'Smith', 10, 3.8),
        (3, 'Bob', 'Johnson', 11, 3.2);
+       
+SELECT * FROM Students
+WHERE Grade = 10;
+
+SELECT Grade, AVG(GPA)
+FROM Students
+GROUP BY Grade;
+
+SELECT Grade, AVG(GPA)
+FROM Students
+GROUP BY Grade
+HAVING AVG(GPA) > 3.5;
+
+SELECT * FROM Students
+ORDER BY LastName;
